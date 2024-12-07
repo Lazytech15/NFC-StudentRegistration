a80f0a19c85196acf4a7cecd144ce8c6b4424efc
Lazytech15 <emmanuelablao16@gmail.com>
2024-12-07 09:33:58 +0800

    add nfcreader to apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import CreateEvent from './Components/Create-event/CreateEvent'
 import StudentRegistration from './Components/Studentregistration/StudentRegistration'
-
+import NFCReader from './Components/NFC-Reader/Nfcreader'
 function App() {
   return (
     <>
       {/* <CreateEvent/> */}
       <StudentRegistration/>
+      <NFCReader/>
     </>
   )
 }
